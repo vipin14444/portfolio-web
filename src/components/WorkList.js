5371abff8cced68d1a609a73b43a48432ec96e9e
vipin14444 <vipin.sharma3039@gmail.com>
2021-09-10 12:13:40 +0530

    Fixed layout on workpage

--- src/components/WorkList.js
+++ src/components/WorkList.js
@@ -1,91 +1,90 @@
-import React, { Component } from 'react'
-import './WorkList.scss'
-import work2 from '../assets/images/work/work2.webp'
-import work5 from '../assets/images/work/work5.png'
-import work6 from '../assets/images/work/work6.png'
-import work7 from '../assets/images/work/work7.png'
-import work8 from '../assets/images/work/work8.png'
-import work9 from '../assets/images/work/work9.png'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import "./WorkList.scss";
+import work2 from "../assets/images/work/work2.webp";
+import work5 from "../assets/images/work/work5.png";
+import work6 from "../assets/images/work/work6.png";
+import work7 from "../assets/images/work/work7.png";
+import work8 from "../assets/images/work/work8.png";
+import work9 from "../assets/images/work/work9.png";
+import { Link } from "react-router-dom";
 
 export default class WorkList extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
             workItems: [
                 {
-                    id: 'closeapp',
-                    title: 'CloseAPP',
+                    id: "shuru",
+                    title: "Shuru",
                     cardImage: work9,
-                    description: 'CloseAPP: Audio Based Social Media'
+                    description: "Shuru: Audio Based Social Media",
                 },
                 {
-                    id: 'walkdesignstudio',
-                    title: 'Walk Design Studio',
+                    id: "walkdesignstudio",
+                    title: "Walk Design Studio",
                     cardImage: work8,
-                    description: 'Walk Design Studio: Business Website'
+                    description: "Walk Design Studio: Business Website",
                 },
                 {
-                    id: 'icurious',
-                    title: 'iCurious',
+                    id: "icurious",
+                    title: "iCurious",
                     cardImage: work7,
-                    description: 'iCurious: E-Learning Platform'
+                    description: "iCurious: E-Learning Platform",
                 },
                 {
-                    id: 'ninja',
-                    title: 'Ninja',
+                    id: "ninja",
+                    title: "Ninja",
                     cardImage: work2,
-                    description: 'Ninja: Online events tracker'
+                    description: "Ninja: Online events tracker",
                 },
                 {
-                    id: 'my-payments',
-                    title: 'My Payments',
+                    id: "my-payments",
+                    title: "My Payments",
                     cardImage: work5,
-                    description: 'Payments Tracker'
+                    description: "Payments Tracker",
                 },
                 {
-                    id: 'commit-ed',
-                    title: 'CommitEd',
+                    id: "commit-ed",
+                    title: "CommitEd",
                     cardImage: work6,
-                    description: 'CommitEd: E-Learning Platform'
+                    description: "CommitEd: E-Learning Platform",
                 },
-            ]
-        }
+            ],
+        };
     }
 
-
     render() {
-        const { workItems } = this.state
+        const { workItems } = this.state;
 
         return (
-            <div className='worklist-cards-container'>
-                {
-                    workItems.map((item, idx) => {
-                        return <WorkCard item={item} key={idx} />
-                    })
-                }
+            <div className="worklist-cards-container">
+                {workItems.map((item, idx) => {
+                    return <WorkCard item={item} key={idx} />;
+                })}
             </div>
-        )
+        );
     }
 }
 
-
 class WorkCard extends Component {
     render() {
-        const { item } = this.props
+        const { item } = this.props;
 
         return (
-            <Link to={`/work/${item.id}`} className='work-card-container'>
+            <Link to={`/work/${item.id}`} className="work-card-container">
                 <div className="top">
                     <h2>{item.title}</h2>
-                    <div style={{ backgroundImage: `url(${item.cardImage})` }} className="bg-img"></div>
+                    <div
+                        style={{ backgroundImage: `url(${item.cardImage})` }}
+                        className="bg-img"
+                    ></div>
                 </div>
                 <div className="bottom">
                     <div className="desc">{item.description}</div>
                     <div className="more-arrow">More</div>
                 </div>
             </Link>
-        )
+        );
     }
 }
--- src/pages/WorkItems.js
+++ src/pages/WorkItems.js
@@ -1,145 +1,121 @@
-import work2 from '../assets/images/work/work2.webp'
-import work5 from '../assets/images/work/work5.png'
-import work6 from '../assets/images/work/work6.png'
-import work7 from '../assets/images/work/work7.png'
-import work8 from '../assets/images/work/work8.png'
-import work9 from '../assets/images/work/work9.png'
+import work2 from "../assets/images/work/work2.webp";
+import work5 from "../assets/images/work/work5.png";
+import work6 from "../assets/images/work/work6.png";
+import work7 from "../assets/images/work/work7.png";
+import work8 from "../assets/images/work/work8.png";
+import work9 from "../assets/images/work/work9.png";
 
 // iCurious
-import ic1 from '../assets/work/iCurious/1.png'
+import ic1 from "../assets/work/iCurious/1.png";
 
 // My Payments
-import mp1 from '../assets/work/myPayments/mp1.png'
-import mp2 from '../assets/work/myPayments/mp2.png'
+import mp1 from "../assets/work/myPayments/mp1.png";
+import mp2 from "../assets/work/myPayments/mp2.png";
 
 // Ninja
-import n1 from '../assets/work/ninja/n1.png'
+import n1 from "../assets/work/ninja/n1.png";
 
 // CommitEd
-import ce1 from '../assets/work/commitEd/ce1.png'
-import ce2 from '../assets/work/commitEd/ce2.png'
-import ce3 from '../assets/work/commitEd/ce3.png'
-import ce4 from '../assets/work/commitEd/ce4.png'
-import ce5 from '../assets/work/commitEd/ce5.png'
-import ce6 from '../assets/work/commitEd/ce6.png'
-import ce7 from '../assets/work/commitEd/ce7.png'
-import ce8 from '../assets/work/commitEd/ce8.png'
-import ce9 from '../assets/work/commitEd/ce9.png'
+import ce1 from "../assets/work/commitEd/ce1.png";
+import ce2 from "../assets/work/commitEd/ce2.png";
+import ce3 from "../assets/work/commitEd/ce3.png";
+import ce4 from "../assets/work/commitEd/ce4.png";
+import ce5 from "../assets/work/commitEd/ce5.png";
+import ce6 from "../assets/work/commitEd/ce6.png";
+import ce7 from "../assets/work/commitEd/ce7.png";
+import ce8 from "../assets/work/commitEd/ce8.png";
+import ce9 from "../assets/work/commitEd/ce9.png";
 
 // Walk Design Studio
-import wds1 from '../assets/work/walkDesignStudio/wds1.png'
-import wds2 from '../assets/work/walkDesignStudio/wds2.png'
-import wds3 from '../assets/work/walkDesignStudio/wds3.png'
-import wds4 from '../assets/work/walkDesignStudio/wds4.png'
-import wds5 from '../assets/work/walkDesignStudio/wds5.png'
+import wds1 from "../assets/work/walkDesignStudio/wds1.png";
+import wds2 from "../assets/work/walkDesignStudio/wds2.png";
+import wds3 from "../assets/work/walkDesignStudio/wds3.png";
+import wds4 from "../assets/work/walkDesignStudio/wds4.png";
+import wds5 from "../assets/work/walkDesignStudio/wds5.png";
 
-// CloseAPP
-import ca1 from '../assets/work/closeapp/ca1.png'
+import shuru1 from "../assets/work/shuru/shuru1.png";
 
 const workItems = [
     {
-        id: 'closeapp',
-        title: 'CloseAPP',
-        projectType: 'Audio Based Social Media Platform',
+        id: "shuru",
+        title: "Shuru",
+        projectType: "Audio Based Social Media Platform",
         titleImage: work9,
-        role: 'Frontend',
+        role: "Frontend",
         projectDescription: [
-            'CloseAPP - Audio Based Social Media Platform.',
-            'CloseAPP is a location based audio primary platform to bring people closer who are in each other’s vicinity in the offline world. ie: People around us. CloseApp aims to empower people to discuss, interact, raise voices on key issues together, and seek help from people around them.',
+            "Shuru - Audio Based Social Media Platform.",
+            "Shuru is a location based audio primary platform to bring people closer who are in each other’s vicinity in the offline world. ie: People around us. Shuru aims to empower people to discuss, interact, raise voices on key issues together, and seek help from people around them.",
         ],
-        screenshots: [
-            ca1
-        ]
+        screenshots: [shuru1],
     },
     {
-        id: 'walkdesignstudio',
-        title: 'Walk Design Studio',
-        projectType: 'Business Website',
+        id: "walkdesignstudio",
+        title: "Walk Design Studio",
+        projectType: "Business Website",
         titleImage: work8,
-        role: 'Frontend',
+        role: "Frontend",
         projectDescription: [
-            'Walk Design Studio - Business Website.',
-            'This website showcases the work of the organisation and the services provided by the organisation.',
-            'It also provides a way for users to connect to the organisation using email.'
+            "Walk Design Studio - Business Website.",
+            "This website showcases the work of the organisation and the services provided by the organisation.",
+            "It also provides a way for users to connect to the organisation using email.",
         ],
-        screenshots: [
-            wds1, wds2, wds3, wds4, wds5
-        ]
+        screenshots: [wds1, wds2, wds3, wds4, wds5],
     },
     {
-        id: 'icurious',
-        title: 'iCurious',
-        projectType: 'E-Learning Platform',
+        id: "icurious",
+        title: "iCurious",
+        projectType: "E-Learning Platform",
         titleImage: work7,
-        role: 'Full Stack',
+        role: "Full Stack",
         projectDescription: [
-            'iCurious is an e-learning platform.',
-            'This platform allows students to post their queries about any subject in the discussion forum so that the teachers as well as other students on the platform can help with the answers.',
-            'The aim of this website is to allow students to book a tuition package from a curated list of teachers. The website is user friendly and easy to use.'
+            "iCurious is an e-learning platform.",
+            "This platform allows students to post their queries about any subject in the discussion forum so that the teachers as well as other students on the platform can help with the answers.",
+            "The aim of this website is to allow students to book a tuition package from a curated list of teachers. The website is user friendly and easy to use.",
         ],
-        screenshots: [
-            ic1
-        ]
+        screenshots: [ic1],
     },
     {
-        id: 'ninja',
-        title: 'Ninja',
-        projectType: 'Online Event Tracker',
+        id: "ninja",
+        title: "Ninja",
+        projectType: "Online Event Tracker",
         titleImage: work2,
-        role: 'Frontend',
+        role: "Frontend",
         projectDescription: [
-            'Ninja - Online event tracker',
-            'Amid this pandemic situation, most of the world is moving towards the online realm.',
-            'Ninja helps users find events that are being held online across various platforms such as YouTube, LinkedIn, Instagram, Facebook and Zoom.'
+            "Ninja - Online event tracker",
+            "Amid this pandemic situation, most of the world is moving towards the online realm.",
+            "Ninja helps users find events that are being held online across various platforms such as YouTube, LinkedIn, Instagram, Facebook and Zoom.",
         ],
-        screenshots: [
-            n1
-        ]
+        screenshots: [n1],
     },
     {
-        id: 'my-payments',
-        title: 'My Payments',
-        projectType: 'Payments Tracker',
+        id: "my-payments",
+        title: "My Payments",
+        projectType: "Payments Tracker",
         titleImage: work5,
-        role: 'Full Stack',
+        role: "Full Stack",
         projectDescription: [
-            'My Payments - Payment tracker is a easy to use transaction tracker.',
-            'This app allows the user to login using their user account and track all the payments that he have made.'
+            "My Payments - Payment tracker is a easy to use transaction tracker.",
+            "This app allows the user to login using their user account and track all the payments that he have made.",
         ],
-        screenshots: [
-            mp1,
-            mp2
-        ]
+        screenshots: [mp1, mp2],
     },
     {
-        id: 'commit-ed',
-        title: 'CommitEd',
-        projectType: 'E-Learning Platform',
+        id: "commit-ed",
+        title: "CommitEd",
+        projectType: "E-Learning Platform",
         titleImage: work6,
-        role: 'Full Stack',
+        role: "Full Stack",
         projectDescription: [
-            'CommitEd is an e-learning platform.',
-            'This platform allows students to watch videos which are categorised based on the college, semester and subject that they want to study.',
-            'The aim of this website is to allow students to access videos of their syllabus easily and all in one place.'
+            "CommitEd is an e-learning platform.",
+            "This platform allows students to watch videos which are categorised based on the college, semester and subject that they want to study.",
+            "The aim of this website is to allow students to access videos of their syllabus easily and all in one place.",
         ],
-        screenshots: [
-            ce1,
-            ce2,
-            ce3,
-            ce4,
-            ce5,
-            ce6,
-            ce7,
-            ce8,
-            ce9,
-        ]
+        screenshots: [ce1, ce2, ce3, ce4, ce5, ce6, ce7, ce8, ce9],
     },
-]
+];
 
 const getWorkItem = (id) => {
-    return workItems.find(x => x.id === id)
-}
+    return workItems.find((x) => x.id === id);
+};
 
-export {
-    getWorkItem
-}
+export { getWorkItem };
